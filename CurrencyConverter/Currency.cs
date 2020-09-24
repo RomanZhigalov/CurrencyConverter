d5ef9a55ec3ff591687eb09c65f1e9a5bd0bb007
Роман Жигалов <raider0084@gmail.com>
2020-09-24 16:50:05 +0700

    StrartupVer.

---
+++ CurrencyConverter/Currency.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+
+namespace Valute
+{
+    public class Currency
+    {
+        public string ID { get; set; }
+        public string NumCode { get; set; }
+        public string CharCode { get; set; }
+        public int Nominal { get; set; }
+        public string Name { get; set; }
+        public float Value { get; set; }
+        public float Previous { get; set; }
+    }
+
+}
